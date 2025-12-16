474b7006c1a86be5900e4e9b6abfdef005f6419d
baqdauletd <baqdaulet.dauletov04@gmail.com>
2025-12-16 14:15:27 +0500

    Initial spring boot project

---
+++ src/main/java/springboot/enterprise_task_service/EnterpriseTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package springboot.enterprise_task_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseTaskServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot/enterprise_task_service/EnterpriseTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.enterprise_task_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
